0a5b512e0f476369eae4d7cad03750c276d15e1b
Conor <conorpp@vt.edu>
2016-03-29 00:33:46 -0400

    .gitignore is now working

--- tools/gencert/info.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-openssl x509 -in $1 -inform der -text
--- tools/verify/bytes.py
+++
@@ -1,7 +0,0 @@
-import fileinput
-import sys
-
-
-line = sys.stdin.read()
-
-sys.stdout.write(''.join([chr(int(''.join(c), 16)) for c in zip(line[0::2],line[1::2])]))

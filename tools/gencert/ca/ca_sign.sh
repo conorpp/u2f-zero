f7d254d207c197b85a60044b500253b7f84b9181
Conor <conorpp@vt.edu>
2017-02-09 23:13:05 -0500

    script for creating not-self-signed cert

---
+++ tools/gencert/ca/ca_sign.sh
@@ -0,0 +1,11 @@
+
+[[ "$#" != 4 ]] && echo "usage: $0 <private-key> <CA-cert> <signing-key> <output-cert>" && exit 1
+
+# generate a "signing request"
+openssl req -new -key "$1" -out "$1".csr
+
+# CA sign the request
+openssl x509 -req -in "$1".csr -CA "$2" -CAkey "$3" -out "$4" -set_serial 0
+
+
+openssl  x509 -in "$4" -outform der -out "$4".der
